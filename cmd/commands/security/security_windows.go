740692dd9248b9a200845b497e24fbff4f8b9e85
papa0four <jsabatini@utexas.edu>
2024-11-17 02:51:01 -0500

    updated OS specific security modules for specific audits

--- cmd/commands/security/security_windows.go
+++ cmd/commands/security/security_windows.go
@@ -7,6 +7,7 @@ package security
 import (
     "fmt"
     "runtime"
+    "strings"
     
     "github.com/spf13/cobra"
     "github.com/papa0four/cpscan/internal/security/audit"
@@ -17,17 +18,36 @@ func init() {
 }
 
 func runWindowsAudit(cmd *cobra.Command, args []string) error {
+    // Determine which checks to run
+    var checks []string
+    if checkSSH {
+        checks = append(checks, "ssh")
+    }
+    if checkFirewall {
+        checks = append(checks, "firewall")
+    }
+    if checkUsers {
+        checks = append(checks, "users")
+    }
+    if checkFilePerms != "" {
+        checks = append(checks, "file-permissions")
+    }
+
     if verbose {
         fmt.Printf("Running security audit for OS: %s\n", runtime.GOOS)
+        if len(checks) > 0 {
+            fmt.Printf("Running checks: %s\n", strings.Join(checks, ", "))
+        }
     }
 
     // Convert command flags to audit options
     opts := audit.AuditOptions{
-        Verbose:     verbose,
-        CustomPaths: customPaths,
-        SkipChecks:  skipChecks,
-        MinSeverity: minSeverity,
-        Timeout:     timeout,
+        Verbose:        verbose,
+        CustomPaths:    customPaths,
+        SkipChecks:     skipChecks,
+        MinSeverity:    minSeverity,
+        Timeout:        timeout,
+        SpecificChecks: checks,
     }
 
     // Create and run auditor
