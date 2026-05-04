aecf871a98d2f0e62b1c287dc49fedd8604ffab9
papa0four <jsabatini@utexas.edu>
2026-05-04 01:01:05 -0400

    updated the global bool to eliminte compile time error for multiple declared globals in a package with same name

--- cmd/commands/osinfo.go
+++ cmd/commands/osinfo.go
@@ -2,30 +2,28 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
-    "github.com/papa0four/cpscan/internal/osfingerprint"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/papa0four/cpscan/internal/osfingerprint"
 )
 
-var verbose bool
+var osinfoVerbose bool
 
 // osinfoCmd represents the command for gathering OS information
 var osinfoCmd = &cobra.Command{
-    Use: "osinfo",
-    Short: "Gather OS Fingerprint information",
-    Long: `osinfo will scan the host machine and retrieve basic OS fingerprint information such as platform, version, and kernel details.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        if verbose {
-            fmt.Println("Verbose mode enabled. Gathering additional information...")
-        }
-        osfingerprint.PrintOSInfo()
-    },
+	Use:   "osinfo",
+	Short: "Gather OS Fingerprint information",
+	Long:  `osinfo will scan the host machine and retrieve basic OS fingerprint information such as platform, version, and kernel details.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if osinfoVerbose {
+			fmt.Println("[*] Verbose mode enabled. Gathering additional information...")
+		}
+		osfingerprint.PrintOSInfo()
+	},
 }
 
 func init() {
-    // Add the osinfo command to the root command
-    RootCmd.AddCommand(osinfoCmd)
-
-    // Define a --verbose flag for osinfo command
-    osinfoCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
+	RootCmd.AddCommand(osinfoCmd)
+	osinfoCmd.Flags().BoolVarP(&osinfoVerbose, "verbose", "v", false, "Enable verbose output")
 }
