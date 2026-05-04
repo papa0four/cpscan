1bafe4352519731a657b46d20382cf1771507234
papa0four <jsabatini@utexas.edu>
2026-05-04 01:14:52 -0400

    removed redundant error handling and unused imports, made main a simply entry point

--- cmd/cpscan/main.go
+++ cmd/cpscan/main.go
@@ -2,16 +2,9 @@
 package main
 
 import (
-    "os"
-    "fmt"
-
-    "github.com/papa0four/cpscan/cmd/commands"
-    _ "github.com/papa0four/cpscan/cmd/commands/security"
-) 
+	"github.com/papa0four/cpscan/cmd/commands"
+)
 
 func main() {
-    if err := cmd.RootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	cmd.Execute()
 }
