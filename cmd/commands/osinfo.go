213c9e4d33f0eae5b68aa1a1a7e6b07914503685
papa0four <sabatini.jesse@gmail.com>
2026-06-17 03:24:29 -0400

    updated hostname in osinfo output and removed osinfo verbose flag

--- cmd/commands/osinfo.go
+++ cmd/commands/osinfo.go
@@ -2,14 +2,10 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"github.com/papa0four/orkowatch/internal/osfingerprint"
 )
-
-var osinfoVerbose bool
 
 // osinfoCmd represents the command for gathering OS information
 var osinfoCmd = &cobra.Command{
@@ -17,14 +13,10 @@ var osinfoCmd = &cobra.Command{
 	Short: "Gather OS Fingerprint information",
 	Long:  `osinfo will scan the host machine and retrieve basic OS fingerprint information such as platform, version, and kernel details.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if osinfoVerbose {
-			fmt.Println("[*] Verbose mode enabled. Gathering additional information...")
-		}
 		osfingerprint.PrintOSInfo()
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(osinfoCmd)
-	osinfoCmd.Flags().BoolVarP(&osinfoVerbose, "verbose", "v", false, "Enable verbose output")
 }
